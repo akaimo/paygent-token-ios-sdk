4714cf8426e4662a8ad764184b49d391f4e2e8d8
shuhei <kawashu0723@gmail.com>
2017-11-17 15:02:12 +0900

    add project

---
+++ paygent-ios-sdk/paygent-ios-sdkTests/paygent_ios_sdkTests.swift
@@ -0,0 +1,36 @@
+//
+//  paygent_ios_sdkTests.swift
+//  paygent-ios-sdkTests
+//
+//  Created by Shuhei Kawaguchi on 2017/11/17.
+//  Copyright © 2017年 Shuhei Kawaguchi. All rights reserved.
+//
+
+import XCTest
+@testable import paygent_ios_sdk
+
+class paygent_ios_sdkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
